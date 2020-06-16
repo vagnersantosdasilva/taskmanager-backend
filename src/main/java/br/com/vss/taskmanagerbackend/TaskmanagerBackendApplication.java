c64fb9682457ed79dd96152d3fd08a1b11e27b12
vagnersantosdasilva <vagnersantosdasilva@gmail.com>
2020-06-16 11:49:50 -0300

    comit inicial

---
+++ src/main/java/br/com/vss/taskmanagerbackend/TaskmanagerBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.vss.taskmanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vss/taskmanagerbackend/TaskmanagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vss.taskmanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
