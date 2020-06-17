2478561832a5511836919f24fb022e7a731040e4
vagnersantosdasilva <vagnersantosdasilva@gmail.com>
2020-06-16 22:50:09 -0300

    Configurando validação para retormar erros corretamente

--- src/main/java/br/com/vss/taskmanagerbackend/TaskmanagerConfiguration.java
+++ src/main/java/br/com/vss/taskmanagerbackend/TaskmanagerConfiguration.java
@@ -1,15 +1,31 @@
 package br.com.vss.taskmanagerbackend;
 
 import br.com.vss.taskmanagerbackend.domain.task.Task;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
-public class TaskmanagerConfiguration  implements RepositoryRestConfigurer {
+public class TaskmanagerConfiguration implements RepositoryRestConfigurer {
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.exposeIdsFor(Task.class);
+    }
+
+    @Bean
+    public Validator validator(){
+        return new LocalValidatorFactoryBean();
+    }
+
+    @Override
+    public void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener validatingListener) {
+        Validator validator = validator();
+        validatingListener.addValidator("beforeCreate",validator);
+        validatingListener.addValidator("beforeSave",validator);
     }
 }
