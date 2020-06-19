79aecc8a542d2c60c6b83d17ca7e90c9fc6aea8c
vagnersantosdasilva <vagnersantosdasilva@gmail.com>
2020-06-19 11:52:07 -0300

    CORs da aplicação configurado

--- src/main/java/br/com/vss/taskmanagerbackend/TaskmanagerConfiguration.java
+++ src/main/java/br/com/vss/taskmanagerbackend/TaskmanagerConfiguration.java
@@ -1,6 +1,8 @@
 package br.com.vss.taskmanagerbackend;
 
 import br.com.vss.taskmanagerbackend.domain.task.Task;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
@@ -12,9 +14,16 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 @Configuration
 public class TaskmanagerConfiguration implements RepositoryRestConfigurer {
 
+    private static final Logger logger = LoggerFactory.getLogger(TaskmanagerBackendApplication.class);
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.exposeIdsFor(Task.class);
+        config.getCorsRegistry()
+                .addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET","POST","PUT","DELETE");
+        logger.info("Repository CORS setup ...OK!");
     }
 
     @Bean
